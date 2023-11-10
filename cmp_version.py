e448f9ec23ff5e97208e7d947b0ed275037749ff
NeverDecaf <jtr9289@gmail.com>
2023-11-10 06:39:33 -0500

    try communicating via a file

--- cmp_version.py
+++ cmp_version.py
@@ -1,7 +1,6 @@
 import json
 from urllib import request
 from pathlib import Path
-import os
 
 url = "https://ak-conf.hypergryph.com/config/prod/official/Android/version"
 server = 'CN'
@@ -17,4 +16,5 @@ json_data = json.loads(content.decode("utf-8"))
 with versionCache.open('r') as file:
     json_data_from_file = json.load(file)
 if json_data['resVersion'] == json_data_from_file[server]['resource'] and json_data['clientVersion'] == json_data_from_file[server]['client']:
-    os.environ['NO_UPDATE_NEEDED'] = 'true'
+    print('Versions are the same, no update needed.')
+    Path('NO_UPDATE_NEEDED').touch()
